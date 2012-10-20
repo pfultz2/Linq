50c66905920373ff41af394b1eed4fbea1b73137
Paul <pfultz2@yahoo.com>
2012-10-20 01:02:24 -0400

    Added last_or_default

--- linq/extensions/last_or_default.h
+++ linq/extensions/last_or_default.h
@@ -9,9 +9,24 @@
 #define LINQ_GUARD_EXTENSIONS_LAST_OR_DEFAULT_H
 
 #include <linq/extensions/extension.h>
+#include <linq/extensions/detail/always.h>
+#include <linq/extensions/last.h>
+#include <boost/range.hpp>
 
 namespace linq { 
+namespace detail {
+struct last_or_default_t
+{
+    template<class Range>
+    auto operator()(Range && r) LINQ_RETURNS(r | linq::last(always(), typename boost::range_value<Range>::type()));
 
+    template<class Range, class Predicate>
+    auto operator()(Range && r, Predicate p) LINQ_RETURNS(r | linq::last(p, typename boost::range_value<Range>::type()));
+};
+}
+namespace {
+range_extension<detail::last_or_default_t> last_or_default = {};
+}
 
 }
 
