fc2d2b406a7fb751b5ef6d567f034a825d9d1c4a
Paul <pfultz2@yahoo.com>
2012-10-20 01:00:52 -0400

    Added last

--- linq/extensions/last.h
+++ linq/extensions/last.h
@@ -9,9 +9,33 @@
 #define LINQ_GUARD_EXTENSIONS_LAST_H
 
 #include <linq/extensions/extension.h>
+#include <boost/range.hpp>
 
 namespace linq { 
+namespace detail {
+struct last_t
+{
+    template<class Iterator, class Predicate, class Value>
+    static typename boost::iterator_value<Iterator>::type last_it(Iterator first, Iterator last, Predicate p, Value && v)
+    {
+        typedef std::reverse_iterator<Iterator> iterator;
+        auto rlast = iterator(last);
+        auto it = std::find_if(iterator(first), rlast, p);
+        if (it == rlast) return v;
+        else return *it;
+    }
 
+    template<class Range, class Predicate, class Value>
+    auto operator()(Range && r, Predicate p, Value && v) LINQ_RETURNS
+    (last_it(boost::begin(r), boost::end(r), p, std::forward<Value>(v)));
+
+    template<class Range>
+    auto operator()(Range && r) LINQ_RETURNS(*(--boost::end(r)));
+};
+}
+namespace {
+range_extension<detail::last_t> last = {};
+}
 
 }
 
