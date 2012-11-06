d3d4f1a04ef4e6795caa17e4f7f6d452208f17a5
Paul <pfultz2@yahoo.com>
2012-11-06 10:32:16 -0500

    Add take_while

--- linq/extensions/take_while.h
+++ linq/extensions/take_while.h
@@ -9,9 +9,29 @@
 #define LINQ_GUARD_EXTENSIONS_TAKE_WHILE_H
 
 #include <linq/extensions/extension.h>
+#include <boost/range.hpp>
+#include <boost/range/algorithm/find_if.hpp>
+#include <linq/extensions/detail/placeholders.h>
 
 namespace linq { 
+namespace detail {
 
+struct not_predicate
+{
+    template<class P, class T>
+    auto operator()(P p, T && x) LINQ_RETURNS(!p(x));
+};
+
+struct take_while_t
+{
+    template<class Range, class Predicate>
+    auto operator()(Range && r, Predicate p) 
+    LINQ_RETURNS(boost::make_reverse_iterator(boost::begin(r), boost::find_if(r, std::bind(not_predicate(), p, _1))));
+};
+}
+namespace {
+range_extension<detail::take_while_t> take_while = {};
+}
 
 }
 
