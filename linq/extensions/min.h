4730ea0c7c9bd45381a7dd55dcc87671416d9ac6
Paul <pfultz2@yahoo.com>
2012-11-23 17:11:14 -0500

    added min

--- linq/extensions/min.h
+++ linq/extensions/min.h
@@ -9,9 +9,30 @@
 #define LINQ_GUARD_EXTENSIONS_MIN_H
 
 #include <linq/extensions/extension.h>
+#include <linq/extensions/aggregate.h>
 
 namespace linq { 
+namespace detail {
 
+struct min_reducer
+{
+    template<class T>
+    T operator()(T x, T y) const
+    {
+        return (x < y) ? x : y;
+    }
+};
+
+struct min_t
+{
+    template<class Range>
+    auto operator()(Range && r) const
+    LINQ_RETURNS(r | linq::aggregate(min_reducer()));
+};
+}
+namespace {
+range_extension<detail::min_t> min = {};
+}
 
 }
 
