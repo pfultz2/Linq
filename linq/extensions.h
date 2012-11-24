a063bc0e653abf844e687733adf3fae0f0c91635
Paul <pfultz2@yahoo.com>
2012-11-24 00:10:15 -0500

    Added extensions header file

---
+++ linq/extensions.h
@@ -0,0 +1,57 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    extensions.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef LINQ_GUARD_LINQ_EXTENSIONS_H
+#define LINQ_GUARD_LINQ_EXTENSIONS_H
+
+#include <linq/extensions/aggregate.h>
+#include <linq/extensions/all.h>
+#include <linq/extensions/any.h>
+#include <linq/extensions/average.h>
+#include <linq/extensions/concat.h>
+#include <linq/extensions/contains.h>
+#include <linq/extensions/count.h>
+#include <linq/extensions/default_if_empty.h>
+#include <linq/extensions/distinct.h>
+#include <linq/extensions/element_at.h>
+#include <linq/extensions/empty_range.h>
+#include <linq/extensions/except.h>
+#include <linq/extensions/extension.h>
+#include <linq/extensions/find.h>
+#include <linq/extensions/first.h>
+#include <linq/extensions/first_or_default.h>
+#include <linq/extensions/group_by.h>
+#include <linq/extensions/group_join.h>
+#include <linq/extensions/intersect.h>
+#include <linq/extensions/join.h>
+#include <linq/extensions/keys.h>
+#include <linq/extensions/last.h>
+#include <linq/extensions/last_or_default.h>
+#include <linq/extensions/max.h>
+#include <linq/extensions/min.h>
+#include <linq/extensions/order_by.h>
+#include <linq/extensions/order_by_descending.h>
+#include <linq/extensions/reverse.h>
+#include <linq/extensions/select.h>
+#include <linq/extensions/select_many.h>
+#include <linq/extensions/sequence_equal.h>
+#include <linq/extensions/single.h>
+#include <linq/extensions/single_or_default.h>
+#include <linq/extensions/skip.h>
+#include <linq/extensions/skip_while.h>
+#include <linq/extensions/sum.h>
+#include <linq/extensions/take.h>
+#include <linq/extensions/take_while.h>
+#include <linq/extensions/then_by.h>
+#include <linq/extensions/then_by_descending.h>
+#include <linq/extensions/to_container.h>
+#include <linq/extensions/to_string.h>
+#include <linq/extensions/values.h>
+#include <linq/extensions/where.h>
+#include <linq/extensions/zip.h>
+
+#endif
