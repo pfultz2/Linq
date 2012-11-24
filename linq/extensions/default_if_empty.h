5d34877422f5de7ffbf8a98dd69308817f95fe51
Paul <pfultz2@yahoo.com>
2012-11-24 15:36:23 -0500

    Restuctucure default_if_empty to only store the value once

--- linq/extensions/default_if_empty.h
+++ linq/extensions/default_if_empty.h
@@ -28,53 +28,71 @@ struct default_if_empty_iterator
     const bool empty;
     Iterator it;
     Value* ref;
-    boost::optional<Value> v;
+    //Value v;
+    //boost::optional<Value> v;
 
-    default_if_empty_iterator(bool empty, Iterator it, Value x)
-    : empty(empty), it(it), v(not empty, x)
-    {}
+    default_if_empty_iterator(bool empty, Iterator it, Value* x)
+    : empty(empty), it(it), ref(x)
+    {
+    }
 
     // TODO: Assign operator
 
     void increment()
     {
         if (not empty) it++;
-        else v = boost::optional<Value>();
+        else ref++;
     }
 
     bool equal(const default_if_empty_iterator<Iterator>& other) const
     {
-        return 
-        (
-            it == other.it and
-            (not (v xor other.v))
-        );
+        assert(this->empty == other.empty);
+        if (this->empty) return ref == other.ref;
+        else return it == other.it;
     }
 
-    typename boost::iterator_reference<Iterator>::type deref() const
+    typename boost::iterator_reference<Iterator>::type dereference() const
     {
         if (not empty) return *it;
-        else return *v;
+        else return *ref;
     }
 };
 
+template<class Iterator, class Value=typename boost::iterator_value<Iterator>::type>
+struct default_if_empty_range : boost::iterator_range<default_if_empty_iterator<Iterator> >
+{
+    typedef boost::iterator_range<default_if_empty_iterator<Iterator> > base;
+    typedef default_if_empty_iterator<Iterator> iterator;
+
+    template<class T>
+    static T* get_end(T* x)
+    {
+        return ++x; 
+    }
+
+    Value v;
+    default_if_empty_range(bool empty, Iterator first, Iterator last, Value x)
+    : v(x), base(iterator(empty, first, &v), iterator(empty, last, get_end(&v)))
+    {}
+};
+
 template<class Iterator, class Value>
-auto make_default_if_empty_iterator(bool empty, Iterator && it, Value && v) LINQ_RETURNS
-(default_if_empty_iterator<Iterator>(empty, it, v));
+default_if_empty_range<Iterator>
+make_default_if_empty_range(bool empty, Iterator first, Iterator last, Value && v)
+{
+    return default_if_empty_range<Iterator>(empty, first, last, v);
+}
 
 template<class Range, class Value>
 auto make_default_if_empty_range(bool empty, Range && r, Value && v) LINQ_RETURNS
-(boost::make_iterator_range
-(
-    make_default_if_empty_iterator(empty, boost::begin(r), v),
-    make_default_if_empty_iterator(empty, boost::end(r), v)
-));
+(make_default_if_empty_range(empty, boost::begin(r), boost::end(r), std::forward<Value>(v)));
+
 
 struct default_if_empty_t
 {
     template<class Range, class T>
     auto operator()(Range && r, T && x) const LINQ_RETURNS
-    (make_default_if_empty_range(boost::empty(r), r, x));
+    (make_default_if_empty_range(boost::empty(r), r, std::forward<T>(x)));
 
     template<class Range, class T>
     auto operator()(Range && r) const LINQ_RETURNS
