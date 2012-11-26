ddd2e96069912aab74a3c58d030d8f24a6aa98ce
Paul <pfultz2@yahoo.com>
2012-11-25 21:19:50 -0500

    Add bind_wrapper to prevent nested bind expressions

--- linq/extensions/extension.h
+++ linq/extensions/extension.h
@@ -48,10 +48,24 @@ pipe_closure<F> make_pipe_closure(F f)
     return pipe_closure<F>(f);
 }
 
-template<class T>
+template<class F>
+struct bind_wrapper : F
+{
+    template<class X>
+    bind_wrapper(X x) : F(x)
+    {}
+};
+
+template<class T, class Enable = void>
 struct auto_ref_type
 {
     typedef T type;
+};
+
+template<class T>
+struct auto_ref_type<T, typename std::enable_if<std::is_bind_expression<T>::value>::type>
+{
+    typedef bind_wrapper<typename std::decay<T>::type> type;
 };
 
 template<class T>
@@ -60,13 +74,13 @@ struct auto_ref_type<T&&>
 {};
 
 template<class T>
-struct auto_ref_type<T&>
+struct auto_ref_type<T&, typename std::enable_if<!std::is_bind_expression<T>::value>::type>
 {
     typedef std::reference_wrapper<T> type;
 };
 
 template<class T>
-struct auto_ref_type<const T&>
+struct auto_ref_type<const T&, typename std::enable_if<!std::is_bind_expression<T>::value>::type>
 {
     typedef std::reference_wrapper<const T> type;
 };
@@ -78,6 +92,28 @@ typename detail::auto_ref_type<T>::type auto_ref(T&& x)
     return typename detail::auto_ref_type<T>::type(std::forward<T>(x));
 }
 
+// template<class T>
+// struct is_bw
+// : boost::mpl::bool_<false>
+// {};
+
+// template<class T>
+// struct is_bw<bind_wrapper<T> >
+// : boost::mpl::bool_<true>
+// {};
+
+// struct foo
+// {
+//     template<class T, class U>
+//     void operator()(T x, U y) const
+//     {
+//     }
+// };
+
+// static_assert(is_bw<decltype(auto_ref(std::bind(foo(), 1, linq::_1)))>::value, "Error");
+// static_assert(!std::is_bind_expression<decltype(auto_ref(std::bind(foo(), 1, linq::_1)))>::value, "Error");
+// static_assert(!is_bw<decltype(auto_ref(1))>::value, "Error");
+
 }
 
 #define LINQ_RANGE_EXTENSION_AUTO_REF(z, n, data) detail::auto_ref(std::forward<T ## n>(x ## n))
@@ -87,7 +123,7 @@ typename detail::auto_ref_type<T>::type auto_ref(T&& x)
     auto operator()(BOOST_PP_ENUM_BINARY_PARAMS_Z(z, n, T, && x) ) const LINQ_RETURNS \
     ( \
         detail::make_pipe_closure \
-        (std::bind(F(), _1, BOOST_PP_ENUM_ ## z(n, LINQ_RANGE_EXTENSION_AUTO_REF, ~) ) ) \
+        (std::bind(F(), linq::_1, BOOST_PP_ENUM_ ## z(n, LINQ_RANGE_EXTENSION_AUTO_REF, ~) ) ) \
     ); 
 
 template<class F>
