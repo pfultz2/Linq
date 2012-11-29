7ee3dfdabf365981a58d1819de85b49fcd734912
paul <paul@paul-TPOWER-X79>
2012-11-29 00:15:35 -0600

    Added more test

--- test.cpp
+++ test.cpp
@@ -33,6 +33,31 @@ struct person
     {}
 };
 
+struct student
+{
+    std::string name;
+    std::vector<int> grades;
+
+    student() {}
+
+    template<class Range>
+    student(std::string name, Range&& r)
+    : name(name), grades(boost::begin(r), boost::end(r))
+    {}
+};
+
+struct pet
+{
+    std::string name;
+    std::string owner;
+
+    pet() {}
+
+    pet(std::string name, std::string owner)
+    : name(name), owner(owner)
+    {}
+};
+
 BOOST_AUTO_TEST_CASE( aggregate_test )
 {
     // the quick brown fox jumps over the lazy dog
@@ -180,11 +205,124 @@ BOOST_AUTO_TEST_CASE( group_by_test )
 
 }
 
+BOOST_AUTO_TEST_CASE( group_join_test )
+{
+    std::vector<person> people = list_of
+    (person("Tom", 25))
+    (person("Bob", 22))
+    (person("Terry", 37))
+    (person("Jerry", 22));
+
+    std::vector<pet> pets = list_of
+    (pet("Barry", "Terry"))
+    (pet("Betty", "Terry"))
+    (pet("Willie", "Bob"))
+    (pet("Dan", "Jerry"));
+
+    // auto q = linq::detail::group_join_t()(people, pets, 
+    //     [](person p) { return p.name; },
+    //     [](pet p) { return p.owner; },
+    //     linq::detail::identity_selector());
+
+    // auto q = people | linq::group_join(pets, 
+    //     [](person p) { return p.name; },
+    //     [](pet p) { return p.owner; },
+    //     linq::detail::identity_selector());
+
+
+}
+
+BOOST_AUTO_TEST_CASE( intersect_test )
+{
+    std::vector<int> v1 = list_of(1)(2)(3)(4)(5);
+    std::vector<int> v2 = list_of(2)(4);
+    std::vector<int> i = list_of(2)(4);
+    BOOST_CHECK(v1 | linq::intersect(v2) | linq::sequence_equal(i));
+}
+
+BOOST_AUTO_TEST_CASE( keys_test )
+{
+    std::map<int, int> m = map_list_of(1, 10)(2, 20)(3, 30);
+    std::vector<int> k = list_of(1)(2)(3);
+    BOOST_CHECK(m | linq::keys | linq::sequence_equal(k));
+}
+
+BOOST_AUTO_TEST_CASE( last_test )
+{
+    std::vector<int> v = list_of(2)(3)(4)(5)(6);
+    BOOST_CHECK_EQUAL(6, v | linq::last);
+    BOOST_CHECK_EQUAL(5, v | linq::last(odd(), 0));
+    BOOST_CHECK_EQUAL(0, v | linq::last([](int x) { return x > 6; } , 0));
+}
+
+BOOST_AUTO_TEST_CASE( last_or_default_test )
+{
+    std::vector<int> v = list_of(2)(3)(4)(5)(6);
+    std::vector<int> empty_v;
+    BOOST_CHECK_EQUAL(6, v | linq::last_or_default);
+    BOOST_CHECK_EQUAL(0, empty_v | linq::last_or_default);
+    BOOST_CHECK_EQUAL(5, v | linq::last_or_default(odd()));
+    BOOST_CHECK_EQUAL(0, v | linq::last_or_default([](int x) { return x > 6; }));
+}
+
+BOOST_AUTO_TEST_CASE( max_test )
+{
+    std::vector<int> v = list_of(2)(3)(8)(1)(4);
+    BOOST_CHECK_EQUAL(8, v | linq::max);
+}
+
+BOOST_AUTO_TEST_CASE( min_test )
+{
+    std::vector<int> v = list_of(2)(3)(8)(1)(4);
+    BOOST_CHECK_EQUAL(1, v | linq::min);
+}
+
+BOOST_AUTO_TEST_CASE( order_by_test )
+{
+    std::vector<person> people = list_of
+    (person("Tom", 25))
+    (person("Bob", 22))
+    (person("Terry", 37))
+    (person("Jerry", 22));
+
+    std::vector<int> people_age = list_of(22)(22)(25)(37);
+
+    auto age_select = [](person p) { return p.age; };
+
+    // BOOST_CHECK(people | linq::order_by(age_select) | linq::select(age_select) | linq::sequence_equal(people_age));
+}
+
+BOOST_AUTO_TEST_CASE( reverse_test )
+{
+    std::vector<int> v1 = list_of(3)(2)(1);
+    std::vector<int> v2 = list_of(1)(2)(3);
+    BOOST_CHECK_EQUAL(1, v1 | linq::reverse | linq::sequence_equal(v2));
+}
+
+BOOST_AUTO_TEST_CASE( select_many_test )
+{
+    std::vector<student> students = list_of
+    (student("Bob", list_of(90)(100)(75)))
+    (student("Tom", list_of(92)(81)(70)))
+    (student("Terry", list_of(105)(98)(94)));
+    std::vector<int> r = list_of(90)(100)(75)
+    (92)(81)(70)
+    (105)(98)(94);
+    BOOST_CHECK(students | linq::select_many([](student s) { return s.grades; }) | linq::sequence_equal(r));
+}
+
 BOOST_AUTO_TEST_CASE( select_test )
 {
     std::vector<int> v = list_of(1)(2)(3)(4);
     std::vector<int> r = list_of(3)(6)(9)(12);
     BOOST_CHECK(v | linq::select([](int i) { return i * 3; }) | linq::sequence_equal(r));
+}
+
+BOOST_AUTO_TEST_CASE( values_test )
+{
+    std::map<int, int> m = map_list_of(1, 10)(2, 20)(3, 30);
+    std::vector<int> v = list_of(10)(20)(30);
+    BOOST_CHECK(m | linq::keys | linq::sequence_equal(v));
 }
 
 
