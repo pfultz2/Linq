5bcfaf72c7ea565ccb9baf0675296b281c4e42b8
Paul <pfultz2@yahoo.com>
2012-12-03 23:33:28 -0500

    Add union

--- linq/extensions/union.h
+++ linq/extensions/union.h
@@ -8,9 +8,22 @@
 #ifndef LINQ_GUARD_EXTENSIONS_UNION_H
 #define LINQ_GUARD_EXTENSIONS_UNION_H
 
+#include <linq/extensions/distinct.h>
+#include <linq/extensions/concat.h>
+
 namespace linq { 
 
-
+namespace detail {
+struct union_t
+{
+    template<class Range1, class Range2>
+    auto operator()(Range1 && r1, Range2 r2) const LINQ_RETURNS
+    (r1 | linq::concat(r2) | linq::distinct);
+};
+}
+namespace {
+range_extension<detail::union_t> union = {};
+}
 
 }
 
