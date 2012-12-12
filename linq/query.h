092a9a02e1d7415e68c13e6d24036becd172f346
paul <paul@paul-TPOWER-X79>
2012-12-12 17:05:18 -0600

    Restucture query parsing

--- linq/query.h
+++ linq/query.h
@@ -10,6 +10,7 @@
 
 #include <linq/pp.h>
 #include <linq/extensions/group_by.h>
+#include <linq/extensions/join.h>
 #include <linq/extensions/select.h>
 #include <linq/extensions/select_many.h>
 #include <linq/extensions/where.h>
@@ -45,6 +46,80 @@ struct select_many
     auto operator+(Sel s) LINQ_RETURNS(linq::select_many(s));
 };
 
+template<class Inner, class OuterSelector, class InnerSelector>
+struct inner_join
+{
+    Inner inner;
+    OuterSelector os;
+    InnerSelector is;
+
+    inner_join(Inner inner, OuterSelector os, InnerSelector is)
+    : inner(inner), os(os), is(is)
+    {}
+
+    template<class Sel>
+    auto operator+(Sel rs) LINQ_RETURNS
+    (linq::join(std::forward<Inner>(inner), os, is, rs));
+};
+
+template<class Inner, class OuterSelector, class InnerSelector>
+inner_join < Inner, OuterSelector, InnerSelector >
+make_inner_join (Inner && inner, OuterSelector os, InnerSelector is)
+{
+    return inner_join < Inner, OuterSelector, InnerSelector >
+    (std::forward<Inner>(inner), os, is);
+}
+
+template<class Inner>
+struct join
+{
+    Inner inner;
+
+    join(Inner inner)
+    : inner(inner)
+    {}
+
+    template<class Pair>
+    auto operator+(Pair p) LINQ_RETURNS
+    (make_inner_join(std::forward<Inner>(inner), p.first, p.second));
+};
+
+template<class Inner>
+join < Inner >
+make_join (Inner && inner)
+{
+    return join < Inner >
+    (std::forward<Inner>(inner));
+}
+
+
+
+
+// template<class Inner, class OuterSelector, class InnerSelector>
+// struct join
+// {
+//     Inner inner;
+//     OuterSelector os;
+//     InnerSelector is;
+
+//     join(Inner inner, OuterSelector os, InnerSelector is)
+//     : inner(inner), os(os), is(is)
+//     {}
+
+//     template<class Sel>
+//     auto operator+(Sel rs) LINQ_RETURNS
+//     (linq::join(std::forward<Inner>(inner), std::forward<OuterSelector>(os), std::forward<InnerSelector>(is), rs));
+// };
+
+// template<class Inner, class OuterSelector, class InnerSelector>
+// join < Inner, OuterSelector, InnerSelector >
+// make_join (Inner && inner, OuterSelector && os, InnerSelector && is)
+// {
+//     return join < Inner, OuterSelector, InnerSelector >
+//     (std::forward<Inner>(inner), std::forward<OuterSelector>(os), std::forward<InnerSelector>(is));
+// }
+
+
 template<class T>
 struct let
 {
@@ -54,14 +129,20 @@ struct let
     template<class F>
     auto operator+(F && f) LINQ_RETURNS(f(std::forward<T>(x)));
 };
+
+
 }
 
 }
+
+#define LINQ_PIPE_I() |
+#define LINQ_PIPE() LINQ_PIPE_I LINQ_EMPTY() ()
 
 // Helps in defining lambdas in two steps. It also dedcues the type held
 // by the container.
 #define LINQ_LAMBDA_BLOCK(...) { return (__VA_ARGS__); }
-#define LINQ_LAMBDA_HEADER(var, col) [&](decltype( linq::query::make_const(*(boost::begin(col))) ) var)
+#define LINQ_LAMBDA_TYPE(col) decltype( linq::query::make_const(*(boost::begin(col))) )
+#define LINQ_LAMBDA_HEADER(var, col) [&](LINQ_LAMBDA_TYPE(col) var)
 #define LINQ_LAMBDA(var, col) LINQ_LAMBDA_HEADER(var, col) LINQ_LAMBDA_BLOCK
 
 // These macros help in defining the clauses. So instead of writing this:
@@ -72,12 +153,12 @@ struct let
 //
 // auto r = numbers | LINQ_WHERE(i, numbers, i > 2);
 //
-#define LINQ_WHERE(var, col, ...) linq::where(LINQ_LAMBDA(var, col)(__VA_ARGS__))
-#define LINQ_SELECT(var, col, ...) linq::select(LINQ_LAMBDA(var, col)(__VA_ARGS__))
-#define LINQ_GROUP(var, col, key, x) linq::group_by(LINQ_LAMBDA(var, col)(key), LINQ_LAMBDA(var, col)(x))
+#define LINQ_WHERE(var, col, ...) LINQ_PIPE() linq::where(LINQ_LAMBDA(var, col)(__VA_ARGS__))
+#define LINQ_SELECT(var, col, ...) LINQ_PIPE() linq::select(LINQ_LAMBDA(var, col)(__VA_ARGS__))
+#define LINQ_GROUP(var, col, key, x) LINQ_PIPE() linq::group_by(LINQ_LAMBDA(var, col)(key), LINQ_LAMBDA(var, col)(x))
 
 
-#define LINQ_ORDERBY(var, col, ...) LINQ_ORDERBY_I(var, col, BOOST_PP_TUPLE_TO_SEQ(LINQ_NARGS(__VA_ARGS__), (__VA_ARGS__)))
+#define LINQ_ORDERBY(var, col, ...) LINQ_PIPE() LINQ_ORDERBY_I(var, col, BOOST_PP_TUPLE_TO_SEQ(LINQ_NARGS(__VA_ARGS__), (__VA_ARGS__)))
 #define LINQ_ORDERBY_I(var, col, seq) LINQ_ORDERBY_EACH(var, col, BOOST_PP_SEQ_HEAD(seq)) LINQ_SEQ_TO_STRING(LINQ_THENBY_EACH(var, col, BOOST_PP_SEQ_TAIL(seq)))
 
 #ifndef _MSC_VER
@@ -92,11 +173,13 @@ struct let
 #define LINQ_ORDERBY_EACH(var, col, x)  LINQ_ORDERBY_METHOD((var, col), LINQ_FIND(LINQ_ORDER_KEYWORD_, x, order_by)) 
 
 #define LINQ_THENBY_EACH(var, col, seq) BOOST_PP_IIF(LINQ_IS_PAREN(seq), BOOST_PP_SEQ_TRANSFORM, LINQ_EAT)(LINQ_THENBY_EACH_OP, (var, col), seq)
-#define LINQ_THENBY_EACH_OP(d, data, x) | LINQ_ORDERBY_METHOD(data, LINQ_FIND(LINQ_THEN_KEYWORD_, x, then_by))  \
+#define LINQ_THENBY_EACH_OP(d, data, x) LINQ_PIPE() LINQ_ORDERBY_METHOD(data, LINQ_FIND(LINQ_THEN_KEYWORD_, x, then_by))  \
 
 
 #define LINQ_SELECT_MANY(var, col) linq::query::select_many() + LINQ_LAMBDA(var, col)
 
+
+#define LINQ_JOIN(ovar, ocol, ivar, icol, os, is) ocol LINQ_PIPE() linq::query::make_inner_join(icol, LINQ_LAMBDA(ovar, ocol)(os), LINQ_LAMBDA(ivar, icol)(is)) + 
 
 
 #define LINQ_LET(var, val) linq::query::let<decltype(val)>(val) + [&](decltype(val) var) LINQ_LAMBDA_BLOCK
@@ -108,7 +191,7 @@ struct let
 #define LINQ_KEYWORD_group (LINQ_GROUP)
 #define LINQ_KEYWORD_into (into)
 #define LINQ_KEYWORD_join (join)
-#define LINQ_KEYWORD_on (on)
+#define LINQ_KEYWORD_on ()
 #define LINQ_KEYWORD_orderby (LINQ_ORDERBY)
 #define LINQ_KEYWORD_select (LINQ_SELECT)
 #define LINQ_KEYWORD_where (LINQ_WHERE)
@@ -120,11 +203,16 @@ struct let
 #define LINQ_ORDER_KEYWORD_descending (order_by_descending)
 
 //
-// Clauses that create scope
+// From keyword
 //
-#define LINQ_SCOPE_KEYWORD_from (LINQ_FROM)
+#define LINQ_FROM_KEYWORD_from ()
 
-#define LINQ_FROM(var, col) col | LINQ_SELECT_MANY(var, col)
+//
+// Join keyword
+//
+#define LINQ_JOIN_KEYWORD_join ()
+
+#define LINQ_FROM(var, col) col LINQ_PIPE() LINQ_SELECT_MANY(var, col)
 
 //
 // Process the sequence
@@ -133,26 +221,25 @@ struct let
 // Expands the parameter, for another preprocessor scan
 #define LINQ_X(...) __VA_ARGS__
 
+// #define LINQ_PROCESS_PAREN(data, x) (LINQ_REM data, LINQ_REM x)
+// #ifndef _MSC_VER
+// #define LINQ_PROCESS_KEYWORD(data, x) | x 
+// #else
+// // MSVC Workarounds
+// #define LINQ_PROCESS_KEYWORD_RES(x) x
+// #define LINQ_PROCESS_KEYWORD(data, x) | LINQ_PROCESS_KEYWORD_RES(x)
+// #endif
+
 #define LINQ_PROCESS_PAREN(data, x) (LINQ_REM data, LINQ_REM x)
-#ifndef _MSC_VER
-#define LINQ_PROCESS_KEYWORD(data, x) | x 
-#else
-// MSVC Workarounds
-#define LINQ_PROCESS_KEYWORD_RES(x) x
-#define LINQ_PROCESS_KEYWORD(data, x) | LINQ_PROCESS_KEYWORD_RES(x)
-#endif
+#define LINQ_PROCESS_KEYWORD(data, x) x 
 
 #define LINQ_COL(var, col) col
 
 // Process the select, where clauses
 #define LINQ_TRANSFORM_CLAUSES(seq) LINQ_TRANSFORM_CLAUSES_TRANSFORM(BOOST_PP_SEQ_ELEM(0, seq) ,BOOST_PP_SEQ_REST_N(1, seq))
-#define LINQ_TRANSFORM_CLAUSES_TRANSFORM(data, seq) LINQ_COL data LINQ_SEQ_TO_STRING(BOOST_PP_SEQ_TRANSFORM(LINQ_TRANSFORM_CLAUSES_O, data, seq))
+#define LINQ_TRANSFORM_CLAUSES_TRANSFORM(data, seq) (LINQ_COL data) BOOST_PP_SEQ_TRANSFORM(LINQ_TRANSFORM_CLAUSES_O, data, seq)
 #define LINQ_TRANSFORM_CLAUSES_O(s, data, x) BOOST_PP_IF(LINQ_IS_PAREN(x), LINQ_PROCESS_PAREN, LINQ_PROCESS_KEYWORD)(data, x)
 
-
-#define LINQ_FOLD_SCOPE(seq) BOOST_PP_SEQ_FOLD_LEFT(op, state, seq) 
-
-#define LINQ_FOLD_SCOPE_OP()
 
 // Process from clauses
 // ()((x, col))()((y, x.col))(LINQ_SELECT)((x))
@@ -161,31 +248,56 @@ struct let
 // TRANSFORM
 // (SELECT_MANY(x, col))((y, x.col)(LINQ_SELECT)(x))
 // NEST
-#define LINQ_TRANSFORM_SCOPE(seq) LINQ_TRANSFORM_SCOPE_CHECK(LINQ_SEQ_SPLIT(seq, LINQ_TRANSFORM_SCOPE_P, data))
-#define LINQ_TRANSFORM_SCOPE_CHECK(seq) BOOST_PP_IF(BOOST_PP_DEC(BOOST_PP_SEQ_SIZE(seq)), LINQ_TRANSFORM_SCOPE_MULTI, LINQ_TRANSFORM_SCOPE_SINGLE)(seq)
-#define LINQ_TRANSFORM_SCOPE_MULTI(seq) LINQ_SEQ_NEST(LINQ_TRANSFORM_SCOPE_TRANSFORM(seq))
-#define LINQ_TRANSFORM_SCOPE_SINGLE(seq) LINQ_TRANSFORM_CLAUSES(BOOST_PP_SEQ_HEAD(seq))
+#define LINQ_TRANSFORM_FROM_CLAUSES(seq) LINQ_SEQ_NEST(LINQ_TRANSFORM_FROM_CLAUSES_EACH(LINQ_SEQ_SPLIT(seq, LINQ_TRANSFORM_FROM_P, data)))
+// #define LINQ_TRANSFORM_FROM_CLAUSES(seq) LINQ_TRANSFORM_FROM_CLAUSES_EACH(LINQ_SEQ_SPLIT(seq, LINQ_TRANSFORM_FROM_P, data))
+// #define LINQ_TRANSFORM_FROM_CLAUSES(seq) LINQ_SEQ_SPLIT(seq, LINQ_TRANSFORM_FROM_P, data)
+// #define LINQ_TRANSFORM_FROM_CLAUSES_EACH(seq) BOOST_PP_SEQ_TRANSFORM(LINQ_TRANSFORM_FROM_CLAUSES_OP, data, seq)
+
+#define LINQ_TRANSFORM_FROM_CLAUSES_EACH(seq) LINQ_TRANSFORM_FROM_CLAUSES_EACH_I(LINQ_TRANSFORM_FROM_CLAUSES_EACH_1 seq)
+#define LINQ_TRANSFORM_FROM_CLAUSES_EACH_I(seq) BOOST_PP_CAT(seq, _END)
+#define LINQ_TRANSFORM_FROM_CLAUSES_EACH_1(x) (LINQ_TRANSFORM_FROM_CLAUSES_OP(x)) LINQ_TRANSFORM_FROM_CLAUSES_EACH_2
+#define LINQ_TRANSFORM_FROM_CLAUSES_EACH_2(x) (LINQ_TRANSFORM_FROM_CLAUSES_OP(x)) LINQ_TRANSFORM_FROM_CLAUSES_EACH_1
+#define LINQ_TRANSFORM_FROM_CLAUSES_EACH_1_END
+#define LINQ_TRANSFORM_FROM_CLAUSES_EACH_2_END
 
 
-#define LINQ_TRANSFORM_SCOPE_TRANSFORM(seq) BOOST_PP_SEQ_TRANSFORM(LINQ_TRANSFORM_SCOPE_OP, data, BOOST_PP_SEQ_POP_BACK(seq)) (LINQ_TRANSFORM_CLAUSES(LINQ_BACK(seq)))
+#define LINQ_TRANSFORM_FROM_CLAUSES_OP(x) LINQ_SEQ_TO_STRING \
+( \
+    BOOST_PP_IF(BOOST_PP_DEC(BOOST_PP_SEQ_SIZE(x)), LINQ_TRANSFORM_FROM_CLAUSES_OTHER, LINQ_TRANSFORM_FROM_CLAUSES_FROM)(x) \
+)
 
-#define LINQ_TRANSFORM_SCOPE_P(s, data, x) LINQ_CONTAINS(LINQ_SCOPE_KEYWORD_, x)
-#ifndef _MSC_VER
-#define LINQ_TRANSFORM_SCOPE_OP(s, data, x) LINQ_FROM LINQ_REM x
-#else
-// MSVC Workarounds
-#define LINQ_TRANSFORM_SCOPE_OP(s, data, x) LINQ_TRANSFORM_SCOPE_OP_INVOKE(LINQ_REM x)
-#define LINQ_TRANSFORM_SCOPE_OP_INVOKE_X(x) x
-#define LINQ_TRANSFORM_SCOPE_OP_INVOKE(x) LINQ_TRANSFORM_SCOPE_OP_INVOKE_X(LINQ_FROM x)
-#endif
+#define LINQ_TRANSFORM_FROM_CLAUSES_FROM(x) (LINQ_FROM) x
+#define LINQ_TRANSFORM_FROM_CLAUSES_OTHER(x) LINQ_TRANSFORM_CLAUSES(x)
+
+// #define LINQ_TRANSFORM_FROM_CHECK(seq) BOOST_PP_IF(BOOST_PP_DEC(BOOST_PP_SEQ_SIZE(seq)), LINQ_TRANSFORM_FROM_MULTI, LINQ_TRANSFORM_FROM_SINGLE)(seq)
+// #define LINQ_TRANSFORM_FROM_MULTI(seq) LINQ_SEQ_NEST(LINQ_TRANSFORM_FROM_TRANSFORM(seq))
+// #define LINQ_TRANSFORM_FROM_SINGLE(seq) LINQ_TRANSFORM_CLAUSES(BOOST_PP_SEQ_HEAD(seq))
+
+
+// #define LINQ_TRANSFORM_FROM_TRANSFORM(seq) BOOST_PP_SEQ_TRANSFORM(LINQ_TRANSFORM_FROM_OP, data, BOOST_PP_SEQ_POP_BACK(seq)) (LINQ_TRANSFORM_CLAUSES(LINQ_BACK(seq)))
+
+#define LINQ_TRANSFORM_FROM_P(s, data, x) LINQ_CONTAINS(LINQ_FROM_KEYWORD_, x)
+// #ifndef _MSC_VER
+// #define LINQ_TRANSFORM_FROM_OP(s, data, x) LINQ_FROM LINQ_REM x
+// #else
+// // MSVC Workarounds
+// #define LINQ_TRANSFORM_FROM_OP(s, data, x) LINQ_TRANSFORM_FROM_OP_INVOKE(LINQ_REM x)
+// #define LINQ_TRANSFORM_FROM_OP_INVOKE_X(x) x
+// #define LINQ_TRANSFORM_FROM_OP_INVOKE(x) LINQ_TRANSFORM_FROM_OP_INVOKE_X(LINQ_FROM x)
+// #endif
 
 
 // Transforms the sequence
-#define LINQ_TRANSFORM(seq) LINQ_X(LINQ_TRANSFORM_SCOPE(seq))
+#define LINQ_TRANSFORM(seq) LINQ_X(LINQ_TRANSFORM_FROM_CLAUSES(seq))
 // And finally the LINQ macro
 #define LINQ(x) LINQ_TRANSFORM(LINQ_TO_SEQ(x))
 
 #ifdef LINQ_TEST_QUERY
+
+// LINQ_NEST
+LINQ_SEQ_NEST((x))
+// LINQ_NEST
+LINQ_SEQ_NEST((f)(y))
 
 // LINQ(from(i, v) select(i * 3))
 LINQ(from(i, v) select(i * 3))
@@ -193,8 +305,8 @@ LINQ(from(i, v) select(i * 3))
 // LINQ(from(x, xx) from(y, yy) select(x * 3))
 LINQ(from(x, xx) from(y, yy) select(x * 3))
 
-// LINQ(from(i, v) select(i * 3) groupby(i))
-LINQ(from(i, v) select(i * 3) groupby(i, i*3))
+// LINQ(from(i, v) select(i * 3) group(i))
+LINQ(from(i, v) select(i * 3) group(i, i*3))
 
 // LINQ(from(i, v) select(i * 3) orderby(i))
 LINQ(from(i, v) select(i * 3) orderby(i))
