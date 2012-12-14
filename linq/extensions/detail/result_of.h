e3541b923719d5f9fc59d035b23fad1faef01c7c
unknown <pfultz2@yahoo.com>
2012-12-14 17:12:00 -0500

    Fixed result_of to correctly introspect the function object

--- linq/extensions/detail/result_of.h
+++ linq/extensions/detail/result_of.h
@@ -1,6 +1,6 @@
 /*=============================================================================
     Copyright (c) 2012 Paul Fultz II
-    auto_result_of.h
+    result_of.h
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
@@ -28,32 +28,47 @@ namespace linq {
 
 #ifdef LINQ_NO_STD_RESULT_OF
 namespace detail {
+
 template<class F>
 struct auto_result_of;
 
-#define LINQ_RESULT_OF_M(z, n, data) linq::declval<T ## n>()
-#define LINQ_RESULT_OF_EACH(z, n, data) \
+#define LINQ_AUTO_RESULT_OF_M(z, n, data) linq::declval<T ## n>()
+#define LINQ_AUTO_RESULT_OF_EACH(z, n, data) \
 template<class X BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_PARAMS_Z(z, n, class T)> \
 struct auto_result_of<X(BOOST_PP_ENUM_PARAMS_Z(z, n, T))> \
 { \
-    typedef decltype(linq::declval<X>()(BOOST_PP_ENUM_ ## z(n, LINQ_RESULT_OF_M, ~) )) type;\
+    typedef decltype(linq::declval<X>()(BOOST_PP_ENUM_ ## z(n, LINQ_AUTO_RESULT_OF_M, ~) )) type;\
 }; 
 
-BOOST_PP_REPEAT_1(LINQ_RESULT_OF_LIMIT, LINQ_RESULT_OF_EACH, ~)
+BOOST_PP_REPEAT_1(LINQ_RESULT_OF_LIMIT, LINQ_AUTO_RESULT_OF_EACH, ~)
 
 BOOST_MPL_HAS_XXX_TEMPLATE_DEF(result)
 
-template<class F>
-struct tr1_result_of
-: F::template result<F>
+
+template<bool HasResult, class F, class Sig>
+struct result_of_impl
+: F::template result<Sig>
 {};
+
+template<class F, class Sig>
+struct result_of_impl<false, F, Sig>
+: auto_result_of<Sig>
+{};
+
 }
 
 
 template<class F>
-struct result_of
-: boost::mpl::eval_if<detail::has_result<F>, detail::tr1_result_of<F>, detail::auto_result_of<F> >
-{};
+struct result_of;
+
+#define LINQ_RESULT_OF_EACH(z, n, data) \
+template<class F BOOST_PP_COMMA_IF(n) BOOST_PP_ENUM_PARAMS_Z(z, n, class T)> \
+struct result_of<F(BOOST_PP_ENUM_PARAMS_Z(z, n, T))> \
+: detail::result_of_impl<detail::has_result<F>::value, F, F(BOOST_PP_ENUM_PARAMS_Z(z, n, T)) > \
+{};  
+
+BOOST_PP_REPEAT_1(LINQ_RESULT_OF_LIMIT, LINQ_RESULT_OF_EACH, ~)
+
 #else
 
 template<class F>
